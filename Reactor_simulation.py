d5c3929110dd3c4b35f123355c6553c618193ad9
SAgaienz <stuartagaienz@gmail.com>
2021-02-16 11:31:51 +0200

    Testing Git

--- Reactor_simulation.py
+++ Reactor_simulation.py
@@ -1,15 +1,16 @@
-#%%
+
+#### Run this to simulate a PBR for the production of ETBE from IB and EtOH...
+#%%\
 import numpy as np 
 import matplotlib.pyplot as plt
-from scipy.integrate import solve_ivp, odeint
+from scipy.integrate import solve_ivp
 from Rates import RATE, EB_rates
 from EnergyBalance import EB
-from StreamData import mt0, Ft0, Q0, P0, sn_ls, fn_ls, F0, phase_check
+from StreamData import  Q0, P0, sn_ls, fn_ls, F0, phase_check
 from PressureDrop import Ergun
 from matplotlib import cm
-from scipy.optimize import fsolve
 from OptimumTemperature import EQ_plot
-from modelling1stRX import Total_RX1_outlet_vals, TotalR1Values
+import pandas
 #%%
 def PBR(z, arr, LD, L, U, Tu, debug=False):
     T, P, Q = arr[:3]
@@ -60,14 +61,15 @@ def reactor_params(Nt, D, Wtot=Wtot, rhob=rhob):
     per_tube_vals = [Wp, Vp, Lp, D, LDp]
     return [Tot_vals, per_tube_vals]
 #%%
-Nt = 500
-D = 75e-3
-T1, P1, Qt1, F1 = Total_RX1_outlet_vals
-T2_0 = 50 + 273.15
-P2_0 = 2000
-Qt2_0 = Qt1
-y0 = per_tube_cond(T2_0, P2_0, Qt2_0, F1, Nt)
-U, Tu = 0, 55+273.15
+Nt = 780
+Fac = 1.5
+D = 52.5e-3
+Ac = (np.pi*D**2)/4
+T1 = 70+273.15
+Pt0, Qt0 = [v['value'] for v in [P0, Q0]]
+Qt0 = Qt0*Fac
+y0 = per_tube_cond(T1, 2200, Qt0, [F*Fac for F in F0], Nt)
+U, Tu = 3.37, 35+273.15
 [Wtot, Vtot, Ltot, D, LDtot], [Wp, Vp, Lp, D, LDp] = reactor_params(Nt, D)
 
 def Reactor_conditions_output():
@@ -77,22 +79,27 @@ def Reactor_conditions_output():
     print('Total reactor length = ' + str(np.round(Ltot, 3)) + ' m')
     print('reactor diameter = ' + str(np.round(D, 3)) + ' m')
     print('L/D tot = ' + str(np.round(LDtot, 3)))
+    print('Ac = ' + str(np.round(Ac, 3)))
     print('--------------- Individual Tube-Reactor Parameters ---------------')
     print('Tube Catalyst mass = ' + str(np.round(Wp, 3)) + ' kg')
     print('Tube reactor volume = ' + str(np.round(Vp, 3)) + ' m3')
     print('Tube reactor length = ' + str(np.round(Lp, 3)) + ' m')
     print('Tube L/D = ' + str(np.round(LDp, 3)))
+    print('Tube Ac = ' + str(np.round(Ac, 3)))
+    print('Qtube = ' + str(y0[2]))
+    print('utube = ' + str(y0[2]/Ac))
 Reactor_conditions_output()
 
 Lspan = np.linspace(0, Lp, 1000)
 debug=True
 ans = solve_ivp(lambda V, arr: PBR(V, arr, LDp, Lp, U, Tu, debug), [0, Lp], y0, dense_output = True).sol(Lspan)
+print('Done R1!')
 #%%
 Tspan = ans[0]
 Pspan = ans[1]
 Qspan = ans[2]
 Fspan = ans[3:]
-
+Rspan = np.array([EB_rates(T, P, Q, Fls) for T, P, Q, Fls in zip(Tspan, Pspan, Qspan, np.array(Fspan).T)]).T
 Tt_ls = Tspan
 Pt_ls = Pspan
 Qt_ls = [Q*Nt for Q in Qspan]
@@ -107,26 +114,41 @@ T1 = Tt_ls[-1]
 P1 = Pt_ls[-1]
 Q1 = Qt_ls[-1]
 F1_ls = [Fls[-1] for Fls in Ft_ls]
-TotalR2Values = [Tt_ls, Pt_ls, Qt_ls, Ft_ls]
-Total_RX2_outlet_vals = [T1, P1, Q1, F1_ls]
-print('Outlet Values:' + str(Total_RX2_outlet_vals))
+TotalR1Values = [Tt_ls, Pt_ls, Qt_ls, Ft_ls]
+op_vals = [Tt_ls, Pt_ls, Qt_ls, *Ft_ls]
+get_vals = True
+Total_RX1_outlet_vals = [T1, P1, Q1, F1_ls]
+print('Outlet Values:' + str([T1, P1, Q1, F1_ls[10], F1_ls[9]]))
+# if get_vals:
+#     kc_R1_span = [kcam_R1(T, P, Q, Fls, D) for T, P, Q, Fls in zip(Tspan, Pspan, Qspan, np.array(Fspan).T)]
+#     df = pandas.DataFrame(kc_R1_span)
+#     df.to_csv('kcam.csv', index = False)
+#%%
+
+df_single = pandas.DataFrame(np.array([Lspan, Tspan, Pspan, Qspan, *Fspan]).T,  columns = ['z', 'T', 'P', 'Q', *sn_ls])
+df_single.to_csv('Single_reactor_values.csv', index = False)
+df_single
 #%%
 Tspan_plot = [T - 273.15 for T in Tspan]
 Pspan_plot = [P for P in Pspan] 
 Fspan_plot = [F*3600 for F in  Fspan]
 
-def plot():
+def plot(Wf = 10, save = True):
+    W =4*2.54
+    H = 1.5*W
     Pcol = 'k'
     Tcol = 'r'
     cols = cm.rainbow(np.linspace(0, 1, len(Fspan_plot)))
     fig, (ax1, axP) = plt.subplots(2,1, sharex = True)
-    for n, Fls, col in zip(fn_ls, Fspan_plot, cols):
+    fig.set_figheight(H)
+    fig.set_figwidth(W)
+    for n, Fls, col in zip(fn_ls, Ft_ls, cols):
         ax1.plot(Lspan, Fls, label = n, color = col)
     ax1.legend(loc = 'best')
-    ax1.set_ylabel('Molar flow (mol/h)')
+    ax1.set_ylabel('Molar flow (mol/s)')
     
     axP.plot(Lspan, Pspan_plot, color = Pcol)
-    axP.set_xlabel('Reactor Length (m)')
+    axP.set_xlabel('Total Equivalent Reactor Length (m)')
     axP.tick_params(axis = 'y', labelcolor = Pcol)
     axP.set_ylabel('Pressure (kPa)', color = Pcol)
 
@@ -134,14 +156,89 @@ def plot():
     axT.plot(Lspan, Tspan_plot, color = Tcol)
     axT.tick_params(axis='y', labelcolor = Tcol)
     axT.set_ylabel(r'Temperature ($^\circ C$)', color = Tcol)
+    plt.tight_layout()
+    if save:
+        plt.savefig('Plots/Main.pdf', dpi = 500)
     plt.show()
-plot()
+plot(True)
 
 #%%
 def TPlot():
     EQ_plot(False)
     plt.plot(TotalR1Values[0], TotalR1Values[3][10])
-    plt.plot(TotalR2Values[0], TotalR2Values[3][10])
     plt.show()
-TPlot()    
+# TPlot()    
+# %%
+
+### determination of where EMT effects are negligable #####
+# for Q, D in zip([])
+nls = ['1. ETBE', '2. TBA1', '3. di-IB', '4. tri-IB', '5. TBA2']
+plt.figure(figsize = [6, 6])
+for rls, n in zip(Rspan, nls):
+    plt.plot(Lspan, rls, label = n)
+# plt.plot(Lspan, kc_R1_span, label =  'kcam')
+plt.xlabel('Length along single reactor tube, m')
+plt.ylabel('Catalyst mass-based rate, mol/kg.s')
+plt.legend(loc = 'best')
+plt.savefig('Plots/AllLines.pdf', dpi = 500)
+plt.show()
+# %%
+def Conversion(Fls):
+    Fls = np.array(Fls).T
+    x_IB = [] # 1
+    x_ETBE = []  # 10
+    x_TBA = [] # 9
+    S_TBA_ETBE = [] 
+    S_DIB_ETBE = []
+    _, F_IB0, _, _, _, _, _, _, F_EtOH0, F_TBA0, F_ETBE0, F_DIB0, _ = Fls[0]
+    # F_ETBE_pos = F_IB + F_ETBE0
+    for _, F_IB, _, _, _, _, _,_,  F_EtOH, F_TBA, F_ETBE, F_DIB, _  in Fls:
+        x_IB.append((F_IB0 - F_IB)/F_IB0)
+        x_ETBE.append(1-((F_IB0 -(F_ETBE - F_ETBE0))/F_IB0) )
+        x_TBA.append(1-((F_IB0 -(F_TBA - F_TBA0))/F_IB0) )
+        S_TBA_ETBE.append((F_TBA - F_TBA0)/(F_ETBE - F_ETBE0))
+        S_DIB_ETBE.append((F_DIB0 - F_DIB0)/(F_ETBE - F_ETBE0))
+    return [x_IB, x_ETBE,x_TBA ,  S_TBA_ETBE, S_DIB_ETBE]
+
+
+# %%
+plt.plot(Lspan, Conversion(Ft_ls)[0], label = 'Total Conversion')
+plt.plot(Lspan, Conversion(Ft_ls)[1], label = 'ETBE Conversion')
+plt.plot(Lspan, Conversion(Ft_ls)[2], label = 'TBA Conversion')
+plt.xlabel('Reactor Length, m')
+plt.ylabel('Conversion')
+plt.show()
+
+# %%
+plt.plot(Lspan, Conversion(Ft_ls)[2])
+plt.plot(Lspan, Conversion(Ft_ls)[3])
+plt.xlabel('Reactor Length, m')
+plt.ylabel(r'Selectivitiy relative to ETBE, $mol_i/mol ETBE$')
+plt.show()
+
+# %%
+Wtot, Vtot, Ltot, Q0['value']*1.5
+# %%
+input_vals = {}
+output_vals = {}
+for i, n in enumerate(['T', 'P', 'Q', 'IB_ane', 'IB', '1B', 'B_diene', 'NB_ane', 'trans_B', 'cis_B', 'water', 'EtOH', 'TBA', 'ETBE', 'di_IB', 'tri_IB']):
+    input_vals[n] = y0[i]
+    output_vals[n] = op_vals[i]
+print(input_vals)
+# %%
+print(sn_ls)
+# %%
+print(sum([input_vals[key] for key in ['IB_ane', 'IB', '1B', 'B_diene', 'NB_ane', 'trans_B', 'cis_B', 'water', 'EtOH', 'TBA', 'ETBE', 'di_IB', 'tri_IB']])*3600)
+# %%
+print(input_vals['Q'])
+# %%
+sum(F0)
+# %%
+np.mean(Tspan)-273.15
+# %%
+Conversion(Ft_ls)[0][-1]
+# %%
+phase_check(134.9+273.15, 19e5)
+# %%
+sn_ls
 # %%
